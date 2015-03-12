bf7d307389637238fab351579331e7e76f10ddd6
Sami Viitanen <sami.viitanen@gmail.com>
2015-03-12 11:50:10 +0200

    Fix Android part so it compiles
    Ok, yet another part actually run against compiler to find mistakes :)

--- src/android/OpenFile.java
+++ src/android/OpenFile.java
@@ -34,46 +34,46 @@ public class OpenFile extends CordovaPlugin {
     return true;
   }
 
-  private static void findIntent(String uri, CallbackContext callback) {
+  private void findIntent(String uri, CallbackContext callback) {
     if(uri == null || uri.isEmpty()) {
-      final String error = "Invalid path " + (uri == null ? "null" : ("'" + uri + "'") + " received.";
+      final String error = "Invalid path " + (uri == null ? "null" : ("'" + uri + "'")) + " received.";
       Log.e(TAG, error);
-      callbackContext.error(error);
+      callback.error(error);
       return;
     }
 
     try {
-        Uri uri = Uri.parse(path);
-        String mime = getMimeTypeFromExtension(path);
+        Uri realUri = Uri.parse(uri);
+        String mime = getMimeTypeFromExtension(uri);
         Intent fileIntent = new Intent(Intent.ACTION_VIEW);
 
         if(Build.VERSION.SDK_INT >= 16) {
-          fileIntent.setDataAndTypeAndNormalize(uri, mime);
+          fileIntent.setDataAndTypeAndNormalize(realUri, mime);
         } else {
-          fileIntent.setDataAndType(uri, mime);
+          fileIntent.setDataAndType(realUri, mime);
         }
 
-        Log.d(TAG, "Starting activity for '" + uri "'...");
+        Log.d(TAG, "Starting activity for '" + uri + "'...");
         cordova.getActivity().startActivity(fileIntent);
 
     } catch (ActivityNotFoundException e) {
       final String error = "Failed to find activity for '" + uri + "'";
       Log.e(TAG, error, e);
-      callbackContext.error(error + ": " + e.getMessage());
+      callback.error(error + ": " + e.getMessage());
     }
 
-    callbackContext.success();
+    callback.success();
   }
 
-  private static String getMimeTypeFromExtension(String uri) {
+  private String getMimeTypeFromExtension(String uri) {
     String mimeType = null;
 
     final String extension = MimeTypeMap.getFileExtensionFromUrl(uri);
     if(extension != null) {
       mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-      LOG.d("Mime type resolved as " + mimeType);
+      Log.d(TAG, "Mime type resolved as " + mimeType);
     } else {
-      LOG.w("Failed to resolve extension from " + uri);
+      Log.w(TAG, "Failed to resolve extension from " + uri);
     }
 
     return mimeType;
